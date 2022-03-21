711e4784cf411ff7a2f34a39d9a5865aed7aa30d
Nadia-ready <nadia.epivent@gmail.com>
2022-03-21 14:37:33 +0100

    add entity

---
+++ src/Entity/Reservations.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ReservationsRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ReservationsRepository::class)]
+class Reservations
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'date')]
+    private $arrival_date;
+
+    #[ORM\Column(type: 'date')]
+    private $departure_date;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getArrivalDate(): ?\DateTimeInterface
+    {
+        return $this->arrival_date;
+    }
+
+    public function setArrivalDate(\DateTimeInterface $arrival_date): self
+    {
+        $this->arrival_date = $arrival_date;
+
+        return $this;
+    }
+
+    public function getDepartureDate(): ?\DateTimeInterface
+    {
+        return $this->departure_date;
+    }
+
+    public function setDepartureDate(\DateTimeInterface $departure_date): self
+    {
+        $this->departure_date = $departure_date;
+
+        return $this;
+    }
+}
