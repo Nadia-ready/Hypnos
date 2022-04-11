5010eadc6d43b45b9a31b446a098b4e153da25d9
Nadia-ready <nadia.epivent@gmail.com>
2022-04-11 18:33:40 +0200

    add catalog of establishment

--- src/Controller/BookingController.php
+++ src/Controller/BookingController.php
@@ -4,8 +4,10 @@ namespace App\Controller;
 
 use App\Entity\Reservations;
 use App\Entity\Suites;
+use App\Form\ReservationsType;
 use App\Repository\EstablishmentsRepository;
 use App\Repository\ReservationsRepository;
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -17,15 +19,37 @@ class BookingController extends AbstractController
     #[Route('/reservationsSuites', name: 'reservationsSuites', methods: ['GET', 'POST'])]
     public function reservationsSuites(ReservationsRepository $reservationsRepository, EstablishmentsRepository $establishmentsRepository): Response
     {
-        return $this->render('reservations/booking.html.twig', [
+        return $this->render('reservations/bookingShow.html.twig', [
             'reservations' => $reservationsRepository->findAll(),
             'establishments' => $establishmentsRepository->findAll()
         ]);
+
     }
     #[Route('/suites/{id}/thumbnail', name: 'thumbnail', methods: ['GET'])]
     public function suiteThumbnail(Request $request, Suites $suite) {
         return $this->render('reservations/thumbnail.html.twig', [
             'suite' => $suite
+        ]);
+    }
+
+    #[Route('/booking/{id}', name: 'booking', methods: ['GET', 'POST'])]
+    public function booking(Request $request, Suites $suites, ReservationsRepository $reservationsRepository, EstablishmentsRepository $establishmentsRepository, UserRepository $userRepository): Response
+    {
+        $reservation = new Reservations();
+        $form = $this->createForm(ReservationsType::class, $reservation);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $reservationsRepository->add($reservation);
+            return $this->redirectToRoute('reservationsSuites', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('reservations/booking.html.twig', [
+            'establishments' => $establishmentsRepository->findAll(),
+            'reservation' => $reservation,
+            'form' => $form,
+
+            'user' => $userRepository->findAll(),
         ]);
     }
 }
---
+++ src/Controller/EstablishmentsCustomerController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Controller;
+
+
+use App\Entity\Establishments;
+use App\Entity\Suites;
+use App\Repository\EstablishmentsRepository;
+use App\Repository\SuitesRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+class EstablishmentsCustomerController extends AbstractController
+{
+    #[Route('/establishments', name: 'customer_establishments_list', methods: ['GET'])]
+    public function establishmentList(EstablishmentsRepository $establishmentsRepository): Response
+    {
+        return $this->render('establishments.html.twig', [
+            'establishments' => $establishmentsRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/establishments/{id}', name: 'customer_establishments_show', methods: ['GET'])]
+    public function show(Establishments $establishment, Suites $suites): Response
+    {
+        return $this->render('establishmentsShow.html.twig', [
+            'establishment' => $establishment,
+            'suite' => $suites,
+        ]);
+    }
+
+
+
+}
